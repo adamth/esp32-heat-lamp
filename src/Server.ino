8936646e5efeddb8d045826db01d6319109afcf9
unknown <adam@adamth.com>
2018-08-31 18:50:17 +1000

    Updates

--- src/Server.ino
+++ src/Server.ino
@@ -3,6 +3,7 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
+int current, last = 0;
 // Replace with your network credentials
 const char *ssid = "OPTUS_35FFAF";
 const char *password = "tunictaxor58193";
@@ -23,11 +24,23 @@ IPAddress gateway(192, 168, 0, 1);
 // Beta coefficient
 #define BCOEFFICIENT 3380
 
+// Emergency shutdown timeout
+// If there is no temerature change after this timeout, shut everything off
+#define EMERGENCYMILLIS 600000 // 10 minutes
+
+// Time to wait before turning lamp on/off again
 #define DEFAULTDEBOUNCE 100000
+
 int targetTemp = 23;
 int debounce = DEFAULTDEBOUNCE;
 bool lightOn = false;
 int currentTemp = 0;
+
+// Timer used to check how long the lamp has been on for
+// and initiate shutdown if timeout expires
+int timer = 0;
+bool timerError = false;
+int lastTemp = 0;
 
 WebServer server(80); //instantiate server at port 80 (http port)
 
@@ -102,133 +115,163 @@ void handleNotFound()
 
 String page()
 {
-  return "<!DOCTYPE html>\
-          <html lang='en'>\
-          <head>\
-            <meta charset='UTF-8'>\
-            <meta name='viewport' content='width=device-width, initial-scale=1.0'>\
-            <meta http-equiv='X-UA-Compatible' content='ie=edge'>\
-            <title>Proving Box Monitor</title>\
-            <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>\
-            <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma-extensions@2.2.1/bulma-slider/dist/css/bulma-slider.min.css'>\
-          </head>\
-          <body>\
-            <div class='light-icon'>\
-              <svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1' id='Capa_1' x='0px' y='0px'\
-                viewBox='0 0 486 486' style='enable-background:new 0 0 486 486;' xml:space='preserve' width='512px' height='512px'>\
-                <path class='light' id='XMLID_49_' d='M298.4,424.7v14.2c0,11.3-8.3,20.7-19.1,22.3l-3.5,12.9c-1.9,7-8.2,11.9-15.5,11.9h-34.7   c-7.3,0-13.6-4.9-15.5-11.9l-3.4-12.9c-10.9-1.7-19.2-11-19.2-22.4v-14.2c0-7.6,6.1-13.7,13.7-13.7h83.5   C292.3,411,298.4,417.1,298.4,424.7z M362.7,233.3c0,32.3-12.8,61.6-33.6,83.1c-15.8,16.4-26,37.3-29.4,59.6   c-1.5,9.6-9.8,16.7-19.6,16.7h-74.3c-9.7,0-18.1-7-19.5-16.6c-3.5-22.3-13.8-43.5-29.6-59.8c-20.4-21.2-33.1-50-33.4-81.7   c-0.7-66.6,52.3-120.5,118.9-121C308.7,113.1,362.7,166.9,362.7,233.3z M256.5,160.8c0-7.4-6-13.5-13.5-13.5   c-47.6,0-86.4,38.7-86.4,86.4c0,7.4,6,13.5,13.5,13.5c7.4,0,13.5-6,13.5-13.5c0-32.8,26.7-59.4,59.4-59.4   C250.5,174.3,256.5,168.3,256.5,160.8z M243,74.3c7.4,0,13.5-6,13.5-13.5V13.5c0-7.4-6-13.5-13.5-13.5s-13.5,6-13.5,13.5v47.3   C229.5,68.3,235.6,74.3,243,74.3z M84.1,233.2c0-7.4-6-13.5-13.5-13.5H23.3c-7.4,0-13.5,6-13.5,13.5c0,7.4,6,13.5,13.5,13.5h47.3   C78.1,246.7,84.1,240.7,84.1,233.2z M462.7,219.7h-47.3c-7.4,0-13.5,6-13.5,13.5c0,7.4,6,13.5,13.5,13.5h47.3   c7.4,0,13.5-6,13.5-13.5C476.2,225.8,470.2,219.7,462.7,219.7z M111.6,345.6l-33.5,33.5c-5.3,5.3-5.3,13.8,0,19.1   c2.6,2.6,6.1,3.9,9.5,3.9s6.9-1.3,9.5-3.9l33.5-33.5c5.3-5.3,5.3-13.8,0-19.1C125.4,340.3,116.8,340.3,111.6,345.6z M364.9,124.8   c3.4,0,6.9-1.3,9.5-3.9l33.5-33.5c5.3-5.3,5.3-13.8,0-19.1c-5.3-5.3-13.8-5.3-19.1,0l-33.5,33.5c-5.3,5.3-5.3,13.8,0,19.1   C358,123.5,361.4,124.8,364.9,124.8z M111.6,120.8c2.6,2.6,6.1,3.9,9.5,3.9s6.9-1.3,9.5-3.9c5.3-5.3,5.3-13.8,0-19.1L97.1,68.2   c-5.3-5.3-13.8-5.3-19.1,0c-5.3,5.3-5.3,13.8,0,19.1L111.6,120.8z M374.4,345.6c-5.3-5.3-13.8-5.3-19.1,0c-5.3,5.3-5.3,13.8,0,19.1   l33.5,33.5c2.6,2.6,6.1,3.9,9.5,3.9s6.9-1.3,9.5-3.9c5.3-5.3,5.3-13.8,0-19.1L374.4,345.6z'\
-                  fill='#FFDA44' />\
-              </svg>\
-            </div>\
-            <section id='error'>\
-              There was an error fetching from the server!\
-            </section>\
-            <section>\
-              <h4>Current Temparature</h4>\
-              <h1 id='currentval'>\
-              </h1>\
-            </section>\
-            <section>\
-              <h4>Target Temparature</h4>\
-              <h1 id='sliderval'>\
-              </h1>\
-              <input id='slider' class='slider is-fullwidth' step='1' min='0' max='34' value='26' type='range'>\
-            </section>\
-          </body>\
-          <script>\
-            const slider = document.querySelector('#slider');\
-            const sliderValue = document.querySelector('#sliderval');\
-            const currentValue = document.querySelector('#currentval');\
-            const light = document.querySelector('.light');\
-            const degrees = '°c';\
-            let errorCount = 0;\
-            sliderValue.innerHTML = slider.value + degrees;\
-            slider.onchange = (e) => {\
-              sliderValue.innerHTML = slider.value + degrees;\
-              fetch('/update', {\
-                method: 'POST',\
-                body: slider.value,\
-              });\
-            };\
-            setInterval(() => {\
-              fetch('/current').then(res => res.json()).then(data => {\
-                currentValue.innerHTML = data.current + degrees;\
-                sliderValue.innerHTML = data.target + degrees;\
-                error.classList.remove('visible');\
-                if (data.lightOn) {\
-                  light.classList.remove('off');\
-                } else {\
-                  light.classList.add('off');\
-                };\
-              }).catch(e => {\
-                if (errorCount > 2) {\
-                  error.classList.add('visible');\
-                  errorCount =0;\
-                } else {\
-                  errorCount++;\
-                }\
-              });\
-            }, 2000);\
-          </script>\
-          <style>\
-            * {\
-              box-sizing: border-box;\
-            }\
-            body {\
-              font-family: 'Roboto';\
-              background: #161a2d;\
-            }\
-            section {\
-              border-radius: 5px;\
-              display: flex;\
-              flex-direction: column;\
-              align-items: center;\
-              padding: 20px;\
-              box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.5);\
-              margin: 20px;\
-              background: #393e4f;\
-              color: white;\
-            }\
-            .slider {\
-              width: 80%;\
-            }\
-            .light-icon {\
-              display: flex;\
-              justify-content: center;\
-            }\
-            svg {\
-              max-height: 20vw;\
-              margin: auto;\
-              max-width: 80px;\
-            }\
-            .off {\
-              fill: grey !important;\
-            }\
-            #error {\
-              background: red;\
-              display: none;\
-            }\
-            #error.visible {\
-              display: block;\
-            }\
-          </style>\
-          </html>";
+  return "<!DOCTYPE html>\n"
+         "<html lang='en'>\n"
+         "\n"
+         "<head>\n"
+         "  <meta charset='UTF-8'>\n"
+         "  <meta name='viewport' content='width=device-width, initial-scale=1.0'>\n"
+         "  <meta http-equiv='X-UA-Compatible' content='ie=edge'>\n"
+         "  <title>Proving Box Monitor</title>\n"
+         "  <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>\n"
+         "  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma-extensions@2.2.1/bulma-slider/dist/css/bulma-slider.min.css'>\n"
+         "</head>\n"
+         "\n"
+         "<body>\n"
+         "  <div class='light-icon'>\n"
+         "    <svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1' id='Capa_1' x='0px' y='0px'\n"
+         "      viewBox='0 0 486 486' style='enable-background:new 0 0 486 486;' xml:space='preserve' width='512px' height='512px'>\n"
+         "      <path class='light' id='XMLID_49_' d='M298.4,424.7v14.2c0,11.3-8.3,20.7-19.1,22.3l-3.5,12.9c-1.9,7-8.2,11.9-15.5,11.9h-34.7   c-7.3,0-13.6-4.9-15.5-11.9l-3.4-12.9c-10.9-1.7-19.2-11-19.2-22.4v-14.2c0-7.6,6.1-13.7,13.7-13.7h83.5   C292.3,411,298.4,417.1,298.4,424.7z M362.7,233.3c0,32.3-12.8,61.6-33.6,83.1c-15.8,16.4-26,37.3-29.4,59.6   c-1.5,9.6-9.8,16.7-19.6,16.7h-74.3c-9.7,0-18.1-7-19.5-16.6c-3.5-22.3-13.8-43.5-29.6-59.8c-20.4-21.2-33.1-50-33.4-81.7   c-0.7-66.6,52.3-120.5,118.9-121C308.7,113.1,362.7,166.9,362.7,233.3z M256.5,160.8c0-7.4-6-13.5-13.5-13.5   c-47.6,0-86.4,38.7-86.4,86.4c0,7.4,6,13.5,13.5,13.5c7.4,0,13.5-6,13.5-13.5c0-32.8,26.7-59.4,59.4-59.4   C250.5,174.3,256.5,168.3,256.5,160.8z M243,74.3c7.4,0,13.5-6,13.5-13.5V13.5c0-7.4-6-13.5-13.5-13.5s-13.5,6-13.5,13.5v47.3   C229.5,68.3,235.6,74.3,243,74.3z M84.1,233.2c0-7.4-6-13.5-13.5-13.5H23.3c-7.4,0-13.5,6-13.5,13.5c0,7.4,6,13.5,13.5,13.5h47.3   C78.1,246.7,84.1,240.7,84.1,233.2z M462.7,219.7h-47.3c-7.4,0-13.5,6-13.5,13.5c0,7.4,6,13.5,13.5,13.5h47.3   c7.4,0,13.5-6,13.5-13.5C476.2,225.8,470.2,219.7,462.7,219.7z M111.6,345.6l-33.5,33.5c-5.3,5.3-5.3,13.8,0,19.1   c2.6,2.6,6.1,3.9,9.5,3.9s6.9-1.3,9.5-3.9l33.5-33.5c5.3-5.3,5.3-13.8,0-19.1C125.4,340.3,116.8,340.3,111.6,345.6z M364.9,124.8   c3.4,0,6.9-1.3,9.5-3.9l33.5-33.5c5.3-5.3,5.3-13.8,0-19.1c-5.3-5.3-13.8-5.3-19.1,0l-33.5,33.5c-5.3,5.3-5.3,13.8,0,19.1   C358,123.5,361.4,124.8,364.9,124.8z M111.6,120.8c2.6,2.6,6.1,3.9,9.5,3.9s6.9-1.3,9.5-3.9c5.3-5.3,5.3-13.8,0-19.1L97.1,68.2   c-5.3-5.3-13.8-5.3-19.1,0c-5.3,5.3-5.3,13.8,0,19.1L111.6,120.8z M374.4,345.6c-5.3-5.3-13.8-5.3-19.1,0c-5.3,5.3-5.3,13.8,0,19.1   l33.5,33.5c2.6,2.6,6.1,3.9,9.5,3.9s6.9-1.3,9.5-3.9c5.3-5.3,5.3-13.8,0-19.1L374.4,345.6z'\n"
+         "        fill='#FFDA44' />\n"
+         "    </svg>\n"
+         "  </div>\n"
+         "  <section id='error'>\n"
+         "    There was an error fetching from the server!\n"
+         "  </section>\n"
+         "  <section>\n"
+         "    <h4>Current Temparature</h4>\n"
+         "    <h1 id='currentval'>\n"
+         "      Loading...\n"
+         "    </h1>\n"
+         "  </section>\n"
+         "  <section>\n"
+         "    <h4>Target Temparature</h4>\n"
+         "    <h1 id='sliderval'>\n"
+         "    </h1>\n"
+         "    <input id='slider' class='slider is-fullwidth' step='1' min='0' max='34' value='26' type='range'>\n"
+         "  </section>\n"
+         "</body>\n"
+         "<script>\n"
+         "  const slider = document.querySelector('#slider');\n"
+         "  const sliderValue = document.querySelector('#sliderval');\n"
+         "  const currentValue = document.querySelector('#currentval');\n"
+         "  const light = document.querySelector('.light');\n"
+         "  const degrees = '°c';\n"
+         "  let sliderActive = false;\n"
+         "  let errorCount = 0;\n"
+         "  sliderValue.innerHTML = slider.value + degrees;\n"
+         "  slider.oninput = (e) => {\n"
+         "    sliderActive = true;\n"
+         "    sliderValue.innerHTML = slider.value + degrees;\n"
+         "  };\n"
+         "  slider.onchange = (e) => {\n"
+         "    sliderActive = false;\n"
+         "    sliderValue.innerHTML = slider.value + degrees;\n"
+         "    fetch('/update', {\n"
+         "      method: 'POST',\n"
+         "      body: slider.value,\n"
+         "    }).catch(e => {\n"
+         "      error.classList.add('visible');\n"
+         "      error.innerHTML = 'There was an error updating the target temperature.';\n"
+         "    });\n"
+         "  };\n"
+         "  setInterval(() => {\n"
+         "    fetch('/current').then(res => res.json()).then(data => {\n"
+         "      currentValue.innerHTML = data.current + degrees;\n"
+         "      if (!sliderActive) {\n"
+         "        slider.value = data.target;\n"
+         "        sliderValue.innerHTML = data.target + degrees;\n"
+         "      }\n"
+         "      if (data.lightOn) {\n"
+         "        light.classList.remove('off');\n"
+         "      } else {\n"
+         "        light.classList.add('off');\n"
+         "      };\n"
+         "      if (data.timerError) {\n"
+         "        error.classList.add('visible');\n"
+         "        error.innerHTML = 'There was a timeout error! The lamp was on but the temperature did not change. Check the warmer and restart the server.';\n"
+         "      } else {\n"
+         "        error.classList.remove('visible');\n"
+         "      };\n"
+         "    }).catch(e => {\n"
+         "      if (errorCount > 2) {\n"
+         "        error.classList.add('visible');\n"
+         "        error.innerHTML = 'There was an error communicating with the server.';\n"
+         "        errorCount = 0;\n"
+         "      } else {\n"
+         "        errorCount++;\n"
+         "      }\n"
+         "    });\n"
+         "  }, 2000);\n"
+         "</script>\n"
+         "<style>\n"
+         "  * {\n"
+         "    box-sizing: border-box;\n"
+         "  }\n"
+         "\n"
+         "  body {\n"
+         "    font-family: 'Roboto';\n"
+         "    background: #161a2d;\n"
+         "  }\n"
+         "\n"
+         "  section {\n"
+         "    border-radius: 5px;\n"
+         "    display: flex;\n"
+         "    flex-direction: column;\n"
+         "    align-items: center;\n"
+         "    padding: 20px;\n"
+         "    box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.5);\n"
+         "    margin: 20px;\n"
+         "    background: #393e4f;\n"
+         "    color: white;\n"
+         "  }\n"
+         "\n"
+         "  .slider {\n"
+         "    width: 80%;\n"
+         "  }\n"
+         "\n"
+         "  .light-icon {\n"
+         "    display: flex;\n"
+         "    justify-content: center;\n"
+         "  }\n"
+         "\n"
+         "  svg {\n"
+         "    max-height: 20vw;\n"
+         "    margin: auto;\n"
+         "    max-width: 80px;\n"
+         "  }\n"
+         "\n"
+         "  .off {\n"
+         "    fill: grey !important;\n"
+         "  }\n"
+         "\n"
+         "  #error {\n"
+         "    background: red;\n"
+         "    display: none;\n"
+         "  }\n"
+         "\n"
+         "  #error.visible {\n"
+         "    display: block;\n"
+         "  }\n"
+         "</style>\n"
+         "\n"
+         "</html>";
 }
 
 void checkTemp()
 {
-  if (debounce == 0)
+  if (debounce == 0 && !timerError)
   {
+    // Save the last temp so we can check that it is going up in the timer function
+    lastTemp = currentTemp;
     currentTemp = getTemp();
     if (currentTemp < targetTemp)
     {
       lightOn = true;
-      digitalWrite(LIGHTPIN, HIGH);
     }
     else
     {
       lightOn = false;
-      digitalWrite(LIGHTPIN, LOW);
     }
     debounce = DEFAULTDEBOUNCE;
   }
@@ -238,8 +281,43 @@ void checkTemp()
   }
 }
 
+void updateTimer()
+{
+  if (lightOn && timer == 0)
+  {
+    // start the timer
+    timer = millis();
+  }
+  else if (lightOn == true && timer > 0 && lastTemp <= currentTemp)
+  {
+    // check to see if the light has been on too long
+    if (millis() - timer > EMERGENCYMILLIS)
+    {
+      lightOn = false;
+      timerError = true;
+    }
+  }
+  else if (!lightOn)
+  {
+    timer = 0;
+  }
+}
+
+void updateLight()
+{
+  if (lightOn && !timerError)
+  {
+    digitalWrite(LIGHTPIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LIGHTPIN, LOW);
+  }
+}
+
 void setup(void)
 {
+  last = millis();
   pinMode(LIGHTPIN, OUTPUT);
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
@@ -267,7 +345,7 @@ void setup(void)
   server.on("/", handleRoot);
 
   server.on("/current", []() {
-    server.send(200, "text/json", "{ \"current\":" + String(currentTemp) + ", \"target\": " + targetTemp + ", \"lightOn\": " + lightOn + "}");
+    server.send(200, "text/json", "{ \"current\":" + String(currentTemp) + ", \"target\": " + targetTemp + ", \"lightOn\": " + lightOn + ", \"timerError\":" + timerError + "}");
   });
 
   server.on("/update", HTTP_POST, []() {
@@ -324,5 +402,7 @@ boolean isNumeric(String str)
 void loop(void)
 {
   checkTemp();
+  updateTimer();
+  updateLight();
   server.handleClient();
 }
